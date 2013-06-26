612e1ed785329315ba2da562851568caea43e978
Clearfield Christopher <chris.clearfield@gmail.com>
2013-06-26 15:40:04 -0400

    Added more hash examples, including a fuction that returns our movie has, and even deeper nested hashes

--- hash.rb
+++ hash.rb
@@ -16,31 +16,56 @@ my_hash["my_name"]="Chris"
 # Throws an error, no variable your_hash
 # puts your_hash["my_name"].inspect
 
-movies={}
-# Title, Year, Director, URL
-my_moon="Goodnight, Moon|2015|Chris Clearfield|www.awesomeimage.com"
-movie_info=my_moon.split("|")
-movie_info_hash={}
-# Symbols can be used as the keys as well!
-# we manually assembled the hash from data in our array.
-movie_info_hash[:title] = movie_info[0]
-movie_info_hash[:year] = movie_info[1]
-movie_info_hash[:director] = movie_info[2]
-movie_info_hash[:movie_url] = movie_info[3]
 
-# WOAH. This is for an array only!
-# movies is a hash!
-# movies << movie_info_hash	
-# Conceptually, this is what I'm going for.  
-# movies["Goodnight, Moon"]
-# : is just used to represent a symbol
-movies[movie_info_hash[:title].to_sym]=movie_info_hash
+def movie_function 
+	# A local variable
+	# Only available in the function
+	movies={}
 
-# This returns a hash!
-puts movies[:"Goodnight, Moon"][:year]
+	movie_list="Goodnight, Moon|2015|Chris Clearfield|www.awesomeimage.com
+On the Waterfront|1982|DiNiro|www.google.com
+Hackers|1996|Angilina Jolie|www.example1.com"
 
+	# File.each. 
+	# Each time we run through the loop, we execute this code
+	movie_list.each_line do |line|
+		# Title, Year, Director, URL
+		movie_info=line.split("|")
 
+		movie_info_hash={}
+		# Symbols can be used as the keys as well!
+		# we manually assembled the hash from data in our array.
+		movie_info_hash[:title] = movie_info[0]
+		# {:released => year} {released: year}
+		# Looking at NESTING HASHES! OMG!!
+		movie_info_hash[:year] = {:released => movie_info[1], :started => {:script_started => 2010, :production_started=>2013}}
+		movie_info_hash[:director] = movie_info[2]
+		movie_info_hash[:movie_url] = movie_info[3]
+		# WOAH. This is for an array only!
+		# movies is a hash!
+		# movies << movie_info_hash	
+		# Conceptually, this is what I'm going for.  
+		# movies["Goodnight, Moon"]
+		# : is just used to represent a symbol
+		movies[movie_info_hash[:title].to_sym]=movie_info_hash
+	end
+	# This is a local variable. We have to return it 
+	# to make accessible from the outside.
+	return movies
+end
 
+#movie_function
 
+# A hash, that is returned from the movie function?
+# WHAT DO I EQUAL?
+puts movie_function[:"Goodnight, Moon"][:year][:started][:production_started]
 
+#<rabbithole>
+# Imaginary github repository info 
+# github = "Chris's repositories"
+# email = github["WDI Summer Public"]["collaborators"]["phlco"]["email_address"]
+# </rabbithole>
 
+#get "/" do
+#	@movies = movie_function
+#end 
